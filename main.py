4021b1296001bb9b5e1e087aa2a3a6c84041f051
wangdongya <896493216@qq.com>
2026-05-10 12:39:44 +0800

    update vector_store's content

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 # main.py
 from loaders.pdf_loader import load_and_split_pdfs
-from vectorstore.chroma_store import get_or_create_vectorstore
+from vectorstore.chroma_store import get_or_create_vectorstore, update_vectorstore_with_pdfs
 from models.llm import create_llm
 from agent.rag_agent import create_rag_agent
 import os, sys
 from config import VECTOR_DB_PATH  # 确保定义了路径，如 "./chroma_db"
 
 def main():
+
     # === 智能加载向量库（只在首次构建）===
-    if not os.path.exists(os.path.join(VECTOR_DB_PATH, "chroma.sqlite3")):
-        print("首次运行：正在加载 PDF 并构建向量库...")
-        all_splits = load_and_split_pdfs()
-        vector_store = get_or_create_vectorstore(documents=all_splits)
-    else:
-        print("加载已有向量库...")
-        vector_store = get_or_create_vectorstore()  # 仅加载
+    vector_store = update_vectorstore_with_pdfs()  # 仅加载
 
     # === 初始化 LLM 和 Agent（传入 vector_store）===
     chat_model = create_llm()
